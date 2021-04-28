da8c4325ef9c62b7f8b8374ce23252add30943e6
Ara <mizrakjian@gmail.com>
2021-04-28 17:04:15 -0400

    Update to modular format

--- aoc_2020/day_05_binary_boarding.py
+++ aoc_2020/day_05_binary_boarding.py
@@ -3,7 +3,6 @@ AoC 2020.5: Binary Boarding
 
 Created on Thu Dec 17 14:18 2020
 """
-print(__doc__.splitlines()[1], "\n")
 
 
 def id_seat(boarding_pass: str) -> int:
@@ -12,11 +11,31 @@ def id_seat(boarding_pass: str) -> int:
     return sum((bit in "BR") << exp for exp, bit in index_bsp)
 
 
-with open("./input/2020_day_5_input.txt") as boarding_passes:
-    seat_ids = {*map(id_seat, boarding_passes)}
+def init() -> set:
+    with open("input/day_05_input.txt") as boarding_passes:
+        seat_ids = {*map(id_seat, boarding_passes)}
+        return seat_ids
 
-seat_min, seat_max = min(seat_ids), max(seat_ids)
-print(f"  Highest seat ID: {seat_max}")
 
-your_seat = (seat_ids ^ {*range(seat_min, seat_max + 1)}).pop()
-print(f"     Your seat ID: {your_seat}")
+def part_1(seat_ids: set) -> int:
+    """Return the max seat ID."""
+    return max(seat_ids)
+
+
+def part_2(seat_ids: set) -> int:
+    """Return "your" seat ID - inside the range of all IDs but not in the set."""
+    seat_min, seat_max = min(seat_ids), max(seat_ids)
+    return (seat_ids ^ {*range(seat_min, seat_max + 1)}).pop()
+
+
+def main():
+    print(__doc__.splitlines()[1], "\n")
+
+    seat_ids = init()
+
+    print(f"  Highest seat ID: {part_1(seat_ids)}")
+    print(f"     Your seat ID: {part_2(seat_ids)}")
+
+
+if __name__ == "__main__":
+    main()
